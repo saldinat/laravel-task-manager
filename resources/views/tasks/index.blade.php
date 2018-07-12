ded8af309bd5c9fb3c4594b29e34a257e596fb69
Tamara Saldina <tsaldina@ayakitchens.local>
2018-07-12 11:53:39 -0400

    'deadline passed', 'due today', 'due tomorrow' added

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -23,50 +23,47 @@
 <h3>Assgined</h3>
 </div>
 @foreach (App\Task::assigned() as $task)
-@if (!$task->completed)
-<div class="row">
-	<div class="col-md-8">
-				<li>User with id {{$task->user_id}}:  <a href="/tasks/{{$task->id}}">{{ $task->title }}</a> created {{ $task->created_at->diffForHumans()}}
-				@if ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 1)
-					<div class="alert alert-warning">1 day left</div>
-				@endif
-	</div>
-						<div class="col-md-4">
-							<form action="/tasks/{{ $task->id }}/complete" method="POST" class="form-inline">
-								{{ csrf_field() }}
-								{{ method_field('PATCH') }}
-								
-								<div class="form-group">
-									<button type="submit" class="btn">
-										<i class="fa {{ $task->completed?'fa-undo':'fa-check' }} " aria-hidden="true"></i>
-									</button>
-								</div>
-							</form>
-							
-							
-							<form action="/tasks/{{ $task->id }}" method="POST">
-								{{ csrf_field() }}
-								{{ method_field('DELETE') }}
-								<button type="submit" class="btn delete" >
-									<i class="fa fa-trash-o" aria-hidden="true"></i>
-								</button>
-							</form>
-						</div>
-						
-				</li>
-</div>
-@if ($task->deadline != null)
-<div class="row">
-	<div class="alert alert-danger" role="alert">
-		Deadline passed {{ date('F d, Y', strtotime($task->deadline)) }}
+	@if (!$task->completed)
+	<div class="row">
+		<div class="col-md-8">
+			<li>User with id {{$task->user_id}}:  <a href="/tasks/{{$task->id}}">{{ $task->title }}</a> created {{ $task->created_at->diffForHumans()}}</li>
+			@if ($task->deadline!= null && $task->deadline->isToday())
+				<div class="alert alert-primary">Due today! </div>
+			@elseif ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 0)
+				<div class="alert alert-warning">Due tomorrow</div>
+			<!--@elseif ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 1)
+				<div class="alert alert-warning">1 day left</div>-->
+			@elseif ((App\Task::where('deadline', '<', Carbon::now())->get())->contains($task))
+				<div class="alert alert-danger" role="alert">
+					Deadline passed {{ date('F d, Y', strtotime($task->deadline)) }}
+				</div>
+			@endif
 		</div>
-</div>
-@endif
-			@endif	
-			@endforeach
+		<div class="col-md-4">
+			<form action="/tasks/{{ $task->id }}/complete" method="POST" class="form-inline">
+				{{ csrf_field() }}
+				{{ method_field('PATCH') }}
+				<div class="form-group">
+					<button type="submit" class="btn">
+						<i class="fa {{ $task->completed?'fa-undo':'fa-check' }} " aria-hidden="true"></i>
+					</button>
+				</div>
+			</form>
+			<form action="/tasks/{{ $task->id }}" method="POST">
+				{{ csrf_field() }}
+				{{ method_field('DELETE') }}
+				<button type="submit" class="btn delete" >
+					<i class="fa fa-trash-o" aria-hidden="true"></i>
+				</button>
+			</form>
+		</div>	
+	</div>
+	@endif	
+@endforeach
 </div>	
 
 <div class="col-md-4">
+@if (count(App\Task::unassigned()) > 0)
 	<div class="row">
 		<h3>Free to-choose</h3>
 	</div>
@@ -74,11 +71,16 @@
 		@if (!$task->completed)
 			<div class="row">
 				<div class="col-md-12">
-					<li><a href="/tasks/{{$task->id}}">{{ $task->title }}</a> created {{ $task->created_at->diffForHumans()}}
-					@if ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 1)
-						<div class="alert alert-warning">1 day left </div>
+					<li><a href="/tasks/{{$task->id}}">{{ $task->title }}</a> created {{ $task->created_at->diffForHumans()}}</li>
+					@if ($task->deadline!= null && $task->deadline->isToday())
+						<div class="alert alert-primary">Due today! </div>
+					@elseif ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 0)
+						<div class="alert alert-warning">Due tomorrow</div>
+					@elseif ((App\Task::where('deadline', '<', Carbon::now())->get())->contains($task))
+						<div class="alert alert-danger" role="alert">
+					Deadline passed {{ date('F d, Y', strtotime($task->deadline)) }}
+				</div>
 					@endif
-					</li>
 				</div>
 			</div>
 			<div class="row">
@@ -108,8 +110,10 @@
 					</form>
 				</div>
 			</div>
+			
 		@endif
 	@endforeach
+	@endif
 	@if (App\Task::count_reserved() > 0)
 	<div class="row">
 		<h3>Reserved</h3>
@@ -117,21 +121,19 @@
 	@foreach (App\Task::reserved() as $task)
 	@if (!$task->completed)
 	<div class="row">
-		<div class="col-md-12">
-			<li><a href="/tasks/{{$task->id}}">{{ $task->title }}</a> reserved {{ $task->updated_at->diffForHumans()}}</li>
-			@if ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 1)
-						<div class="alert alert-warning">1 day left </div>
-					@endif
-		</div>
-	</div>
-	<div class="row">
 		<div class="col-md-8">
-		<div class="form-group">
-		
-		reserved by user #{{ $task->reserved_by }} </li>
-
+			<li><a href="/tasks/{{$task->id}}">{{ $task->title }}</a> reserved by user #{{ $task->reserved_by }} {{ $task->updated_at->diffForHumans()}}</li>
+			@if ($task->deadline!= null && $task->deadline->isToday())
+				<div class="alert alert-primary">Due today! </div>
+			@elseif ($task->deadline!= null && $task->deadline->diffInDays(Carbon::now()) == 0)
+				<div class="alert alert-warning">Due tomorrow</div>
+			@elseif ((App\Task::where('deadline', '<', Carbon::now())->get())->contains($task))
+				<div class="alert alert-danger" role="alert">
+					Deadline passed {{ date('F d, Y', strtotime($task->deadline)) }}
+				</div>
+			@endif
 		</div>
-		</div>
+	
 		<div class="col-md-4">				
 		<form action="/tasks/{{ $task->id }}/complete" method="POST" class="form-inline">
 		{{ csrf_field() }}
@@ -149,8 +151,8 @@
 		<i class="fa fa-trash-o" aria-hidden="true"></i>
 		</button>
 		</form>
-		</div>
-	</div>
+		</div></div>
+	
 
 	
 	@endif
@@ -195,17 +197,4 @@
 
 </div>
 
-<!--<div class="row">
-	<div class="col-md-4">
-		<div class="row">
-		<h3>Deadline passed</h3>
-		@foreach (App\Task::where('deadline', '<', Carbon::now()->toDateString())->get() as $t)
-		<div class="alert alert-danger" role="alert">
-		<a href="/tasks/{{$task->id}}">{{ $t->title }}</a>
-		{{ date('F d, Y', strtotime($t->deadline)) }}
-		</div>
-		@endforeach
-	</div>
-	</div>
-</div> -->
 @endsection
